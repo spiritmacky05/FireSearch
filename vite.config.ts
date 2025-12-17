345a14489a5d57ef29dce5e31d1b940e180e171e
spiritmacky05 <spiritmacky05@gmail.com>
2025-12-18 01:01:20 +0800

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => {
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.AIzaSyDDK2ofi034k1hYKV-BLkSmRZuTpN17K7o)
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
       resolve: {
         alias: {
